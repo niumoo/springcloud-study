bce55ec3203ee10416d328df9e04095b17804c7a
niumoo <niumoo@126.com>
2020-03-14 15:18:03 +0800

    eureka-client

---
+++ eureka-client/src/test/java/com/wdbyte/eureka/client/EurekaClientApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wdbyte.eureka.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
