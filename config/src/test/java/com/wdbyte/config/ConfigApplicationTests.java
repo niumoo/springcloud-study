a776124f649b88cba4a3df75f4d6b8a3991cac7f
niumoo <niumoo@126.com>
2020-03-22 20:02:06 +0800

    :tada: spring cloud config

---
+++ config/src/test/java/com/wdbyte/config/ConfigApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wdbyte.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
